ba9ccdbe41b2892b345a5ab7e7da1581188294a6
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-02-13 23:11:50 +0200

    Make navbar responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul className="menu">
-      <li className="menu-link"><Link className="menu-link-l" to="/">Home</Link></li>
-      <li className="menu-link"><Link className="menu-link-l" to="/about">About</Link></li>
-      <li className="menu-link"><Link className="menu-link-l" to="/projects">Projects</Link></li>
-      <li className="menu-link"><Link className="nav-name" to="/">AZ</Link></li>
-      <li className="menu-link"><Link className="menu-link-l" to="/education">Education</Link></li>
-      <li className="menu-link"><Link className="menu-link-l" to="/blog">Blog</Link></li>
-      <li className="menu-link"><Link className="menu-link-l" to="/contact">Contact</Link></li>
-    </ul>
-  </nav>
-);
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({ clicked: !prevState.clicked }));
+  };
+
+  render() {
+    const { clicked } = this.state;
+
+    return (
+      <>
+        <nav id="navbar" className={clicked ? '#navbar active' : '#navbar'}>
+          <ul className="menu">
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/projects">
+                Projects
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="nav-name" to="/">
+                AZ
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/education">
+                Education
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li className="menu-link">
+              <Link className="menu-link-l" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div
+          id="mobile"
+          onClick={this.handleClick}
+          onKeyPress={this.handleKeyPress}
+          role="button"
+          tabIndex={0}
+        >
+          <i id="bar" className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default Navbar;
